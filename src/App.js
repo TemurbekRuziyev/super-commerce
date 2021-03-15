17c18267d2e99adb7cce2aee8fb5eb5a3b711991
Temurbek Ro'ziyev <68279922+TemurbekRoziyev@users.noreply.github.com>
2021-03-16 02:15:40 +0500

    Setting up router, code splitting, error-boundary to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import { AppContainer, GlobalStyles } from './App.styles';
+
+import SidebarMenu from './components/sidebar-menu/sidebar-menu.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
+
+const HomePage = lazy(() => import('./pages/homePage/homePage.component'));
+const ShopPage = lazy(() => import('./pages/shopPage/shopPage.component'));
+const ProfilePage = lazy(() => import('./pages/profilePage/profile.component'));
+const CheckoutPage = lazy(() => import('./pages/checkoutPage/checkout.component'));
+
+const App = () => {
   return (
-    <div className='App'>
-    </div>
+    <AppContainer>
+      <GlobalStyles />
+      <BrowserRouter>
+        <SidebarMenu />
+        <Switch>
+          <ErrorBoundary>
+            <Suspense fallback={<div>...Loading</div>}>
+              <Route exact path='/' component={HomePage} />
+              <Route exact path='/shop' component={ShopPage} />
+              <Route exact path='/user' component={ProfilePage} />
+              <Route exact path='/checkout' component={CheckoutPage}/>
+            </Suspense>
+          </ErrorBoundary>
+        </Switch>
+      </BrowserRouter>
+    </AppContainer>
   );
-}
+};
 
 export default App;
