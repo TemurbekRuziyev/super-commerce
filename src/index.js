5f4146780446e26e9a34fd83a1cdbd58581268e0
Temurbek Ro'ziyev <68279922+TemurbekRoziyev@users.noreply.github.com>
2021-03-17 00:56:30 +0500

    Binding app with redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+import {store} from './redux/store';
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
