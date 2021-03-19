59398c4e897f59a0ca186999796e23c4a90cbc43
Temurbek Ro'ziyev <68279922+TemurbekRoziyev@users.noreply.github.com>
2021-03-19 22:37:03 +0500

    Implemented redux-persist for todo app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root.reducer';
 import { rootSaga } from './root.saga';
@@ -15,3 +16,5 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
