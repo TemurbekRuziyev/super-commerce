60da1495b6df611a68a838f83f800c3f9eb039aa
Temurbek Ro'ziyev <68279922+TemurbekRoziyev@users.noreply.github.com>
2021-03-19 22:34:01 +0500

    Created todo app in homepage in order to convenient

--- src/pages/homePage/homePage.component.jsx
+++ src/pages/homePage/homePage.component.jsx
@@ -5,16 +5,47 @@ import { createStructuredSelector } from 'reselect';
 import { selectCategoryList } from '../../redux/homepage-collection/homepage-collection.selector';
 import { fetchCategoryStart } from '../../redux/homepage-collection/homepage-collection.actions';
 
-import { HomePageContainer } from './homePage.styles';
+import {
+  HomePageContainer,
+  HomePageHeading,
+  RecommendedProducts,
+  BestSellerProducts,
+  BuyScheduleContainer,
+  ScheduleContainer,
+  HomePageText
+} from './homePage.styles';
+import Schedule from '../../components/schedule/schedule.component';
 
 const HomePage = ({ fetchCategory, category }) => {
   useEffect(() => {
     fetchCategory();
   }, [fetchCategory]);
+
   console.log(category);
+
   return (
     <HomePageContainer>
-      <h1>HomePage</h1>
+      <HomePageHeading>
+        Good afternoon Temurbek, <br /> Welcome to Super-Commerce Website
+      </HomePageHeading>
+      <RecommendedProducts>
+        <HomePageText>
+          Recommended Products
+        </HomePageText>
+      </RecommendedProducts>
+      <BestSellerProducts>
+        <HomePageText>
+          Top Saled Products
+        </HomePageText>
+      </BestSellerProducts>
+      <BuyScheduleContainer>
+        <HomePageText>
+          Let's create your schedule purchasing
+        </HomePageText>
+        <ScheduleContainer>
+          <Schedule />
+        </ScheduleContainer>
+      </BuyScheduleContainer>
     </HomePageContainer>
   );
 };
--- src/pages/homePage/homePage.styles.jsx
+++ src/pages/homePage/homePage.styles.jsx
@@ -1,6 +1,35 @@
 import styled from 'styled-components';
 
 export const HomePageContainer = styled.div`
+  padding: 1rem;
   position: relative;
   grid-column: 2 / 3;
+`;
+
+export const HomePageHeading = styled.h2`
+  font-size: 2.5rem;
+  background-clip: text;
+  -webkit-background-clip: text;
+  background-image: linear-gradient(to right, yellow, red);
+  color: transparent;
+`;
+
+export const RecommendedProducts = styled.div`
+
+`;
+
+export const BestSellerProducts = styled.div`
+
+`;
+
+export const BuyScheduleContainer = styled.div`
+
+`;
+
+export const HomePageText = styled.h2`
+  font-size: 2rem;
+`;
+
+export const ScheduleContainer = styled.div`
+  
 `;
