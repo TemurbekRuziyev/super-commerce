4dfdf671f72cc21d3fe7df81c9f94b459ead7b1b
Temurbek Ro'ziyev <68279922+TemurbekRoziyev@users.noreply.github.com>
2021-03-26 22:26:23 +0500

    Implemented stripe checkout button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -6,12 +6,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51IO5OBAkiXt2qprphnG1MRBUbA7lO40TurxA0bgDHN8ep8t8dZnoxYaxpeSsenAIrEYlWMc5UFbD6j737BPlbTnG00jKICvOXe';
   const priceForStripe = 100 * price;
 
+  const onToken = token => {
+    alert(token);
+  };
 
   return (
-  <StripeCheckout 
-    stripeKey={stripeKey}  
-    name='Super-commerce payment'
-      
-  />
+    <StripeCheckout
+      stripeKey={stripeKey}
+      amount={priceForStripe}
+      name='Super-commerce payment'
+      label='Pay now easily'
+      currency='USD'
+      billingAddress
+      shippingAddress
+      allowRememberMe
+      token={onToken}
+    />
   );
 };
+
+export default StripeCheckoutButton;
